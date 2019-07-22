45705daba69b7c562801905a98e4322c39d50dd4
fangbin <jsonp@sina.com>
2019-07-22 15:56:23 +0800

    a

--- config/params.php
+++ config/params.php
@@ -8,7 +8,7 @@ if (YII_ENV == 'dev') {
         'appid' => 'wx547bae972d6fecf9',
         'appsercet' => '1f391c7cedf7862260078eba1924d31d',
         'randkey' => 'GR511M',
-        'loginCacheTime' => 86400, //登陆状态记录时间，单位为秒
+        'loginCacheTime' => 864000, //登陆状态记录时间，单位为秒
         'withoutlogin' => ['login'],
         'serverHost' => 'http://www.wechatserver.com/'
     ];
@@ -20,7 +20,7 @@ if (YII_ENV == 'dev') {
         'appid' => 'wx547bae972d6fecf9',
         'appsercet' => '1f391c7cedf7862260078eba1924d31d',
         'randkey' => 'GR511M',
-        'loginCacheTime' => 86400, //登陆状态记录时间，单位为秒
+        'loginCacheTime' => 864000, //登陆状态记录时间，单位为秒
         'withoutlogin' => ['login'],
         'serverHost' => 'https://jifen.myshared.top/'
     ];
