ff2eb405ab8463bf1018bebd7c2214cb63511a9c
fangbin <jsonp@sina.com>
2019-08-28 10:49:49 +0800

    a

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\RoomUsers;
 use app\models\Users;
 use Yii;
 use yii\filters\AccessControl;
@@ -18,10 +19,17 @@ class TestController extends Controller
 
     public function actionTest() {
 
-        $a = '1.9.0';
+        $RoomUsers = RoomUsers::find()->where(['id'=>258])->one();
+
+        $name = Users::getNickname(1738);
+
+        $RoomUsers->nickname = $name;
+        $RoomUsers->update_time = date('Y-m-d H:i:s');
+        $RoomUsers->save();
+        $a = '2.0.0';
         $b = '1.9.0';
         echo vesionInt($a);
-        var_dump($a === $b);
+        var_dump(vesionInt($a) >= vesionInt($b));
 
     }
 
