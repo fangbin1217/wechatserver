3a676e68537a984a7fea53d53282f5538eb4583b
fangbin <jsonp@sina.com>
2019-10-24 11:05:38 +0800

    a

--- common/helpers.php
+++ common/helpers.php
@@ -43,9 +43,17 @@ if (! function_exists('getRandData')) {
     function getRandData($check = true) {
 
         $boxClass = ['out-front img-out', 'out-back img-out', 'out-left img-out', 'out-right img-out', 'out-top img-out', 'out-bottom img-out'];
-        $data = [
-            'shu.png', 'niu.png', 'hu.png', 'tu.png', 'long.png', 'she.png'
-        ];
+
+        if (Yii::$app->params['isChecked']) {
+            $data = [
+                'shu.png', 'niu.png', 'hu.png', 'tu.png', 'long.png', 'she.png'
+            ];
+        } else {
+            $data = [
+                'a1.png', 'a2.png', 'a3.png', 'a4.png', 'a5.png', 'a6.png'
+            ];
+        }
+
         $datas = [];
         $i = 0;
         foreach ($data as $val) {
